09aaa64b158b4c9842fe49c1d16494820d93bc42
Leigh Caplan <texel1@gmail.com>
2016-01-23 13:13:57 -0800

    Add shabu dependency

--- src/app/ng-808.component.ts
+++ src/app/ng-808.component.ts
@@ -4,7 +4,6 @@ import {FORM_PROVIDERS} from 'angular2/common';
 @Component({
   selector: 'ng-808',
   template: require('./ng-808.html')
-  // template: 'ng808!'
 })
 export class Ng808 {
   constructor() {
