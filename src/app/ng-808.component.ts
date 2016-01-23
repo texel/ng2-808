2d4c9192105852eedffbffcd7cef15675427c8e2
Leigh Caplan <texel1@gmail.com>
2016-01-23 13:08:22 -0800

    First commit

---
+++ src/app/ng-808.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {FORM_PROVIDERS} from 'angular2/common';
+
+@Component({
+  selector: 'ng-808',
+  template: require('./ng-808.html')
+  // template: 'ng808!'
+})
+export class Ng808 {
+  constructor() {
+    console.debug('ng808!');
+  }
+}
